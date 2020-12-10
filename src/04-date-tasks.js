c8d64d27ebdb5755d3c89e346bcaa851db48726e
Bogush-Dima <68421669+Bogush-Dima@users.noreply.github.com>
2020-12-10 22:50:36 +0300

    feat: some answers

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -7,74 +7,78 @@
  ******************************************************************************************* */
 
 
-/**
- * Parses a rfc2822 string date representation into date value
- * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
- *
- * @param {string} value
- * @return {date}
- *
- * @example:
- *    'December 17, 1995 03:24:00'    => Date()
- *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
- *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
- */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
-}
-
-/**
- * Parses an ISO 8601 string date representation into date value
- * For ISO 8601 date specification refer to : https://en.wikipedia.org/wiki/ISO_8601
- *
- * @param {string} value
- * @return {date}
- *
- * @example :
- *    '2016-01-19T16:07:37+00:00'    => Date()
- *    '2016-01-19T08:07:37Z' => Date()
- */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  const parsed = Date.parse(value);
+  const res = new Date(parsed);
+  return res;
 }
 
 
-/**
- * Returns true if specified date is leap year and false otherwise
- * Please find algorithm here: https://en.wikipedia.org/wiki/Leap_year#Algorithm
- *
- * @param {date} date
- * @return {bool}
- *
- * @example :
- *    Date(1900,1,1)    => false
- *    Date(2000,1,1)    => true
- *    Date(2001,1,1)    => false
- *    Date(2012,1,1)    => true
- *    Date(2015,1,1)    => false
- */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  const parsed = Date.parse(value);
+  const res = new Date(parsed);
+  return res;
 }
 
 
-/**
- * Returns the string represention of the timespan between two dates.
- * The format of output string is "HH:mm:ss.sss"
- *
- * @param {date} startDate
- * @param {date} endDate
- * @return {string}
- *
- * @example:
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,11,0,0)   => "01:00:00.000"
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,30,0)       => "00:30:00.000"
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,20)        => "00:00:20.000"
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
- *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
- */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  let result = false;
+  const year = date.getFullYear();
+  const yearStr = year.toString();
+  const firstHalf = yearStr.slice(0, 2);
+  const lastHalf = yearStr.slice(-2);
+  if (firstHalf % 4 === 0 && lastHalf % 4 === 0) {
+    result = true;
+  }
+  return result;
+}
+
+
+function timeSpanToString(startDate, endDate) {
+  const startHours = startDate.getHours();
+  const startMinutes = startDate.getMinutes();
+  const startSeconds = startDate.getSeconds();
+  const startMilliseconds = startDate.getMilliseconds();
+  const endHours = endDate.getHours();
+  const endMinutes = endDate.getMinutes();
+  const endSeconds = endDate.getSeconds();
+  const endMilliseconds = endDate.getMilliseconds();
+  let resultHours = 0;
+  let resultMinutes = 0;
+  let resultSeconds = 0;
+  let resultMilliseconds = 0;
+  let strHours;
+  let strMinutes;
+  let strSeconds;
+  let strMilliseconds;
+  resultHours = Math.abs(startHours - endHours);
+  resultMinutes = Math.abs(startMinutes - endMinutes);
+  resultSeconds = Math.abs(startSeconds - endSeconds);
+  resultMilliseconds = Math.abs(startMilliseconds - endMilliseconds);
+  if (resultHours < 10) {
+    strHours = `0${resultHours}`;
+  } else {
+    strHours = `${resultHours}`;
+  }
+  if (resultMinutes < 10) {
+    strMinutes = `0${resultMinutes}`;
+  } else {
+    strMinutes = `${resultMinutes}`;
+  }
+  if (resultSeconds < 10) {
+    strSeconds = `0${resultSeconds}`;
+  } else {
+    strSeconds = `${resultSeconds}`;
+  }
+  if (resultMilliseconds < 10) {
+    strMilliseconds = `00${resultMilliseconds}`;
+  } else if (resultMilliseconds < 100) {
+    strMilliseconds = `0${resultMilliseconds}`;
+  } else {
+    strMilliseconds = `${resultMilliseconds}`;
+  }
+  const result = `${strHours}:${strMinutes}:${strSeconds}.${strMilliseconds}`;
+  return result;
 }
 
 
