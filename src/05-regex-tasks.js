cd5eddf7fc906f4efe9d6a833a9b83f93bb30bca
Bogush-Dima <68421669+Bogush-Dima@users.noreply.github.com>
2020-12-14 10:28:10 +0300

    feat: some answers

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -9,30 +9,8 @@
  ******************************************************************************************* */
 
 
-/**
- * Returns the regexp that matches a GUID string representation
- * '{XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}',
- * where X is hexadecimal digit (0,1,2...,9,A,a,B,b,C,c,D,d,F,f)
- *
- * See more details: https://en.wikipedia.org/wiki/Globally_unique_identifier
- *
- * Match :
- *   '{3F2504E0-4F89-41D3-9A0C-0305E82C3301}'
- *   '{21EC2020-3AEA-4069-A2DD-08002B30309D}'
- *   '{0c74f13f-fa83-4c48-9b33-68921dd72463}'
- *
- *  Do not match:
- *   '{D44EF4F4-280B47E5-91C7-261222A59621}'
- *   '{D1A5279D-B27D-4CD4-A05E-EFDH53D08E8D}'
- *   '{5EDEB36C-9006-467A8D04-AFB6F62CD7D2}'
- *   '677E2553DD4D43B09DA77414DB1EB8EA'
- *   '0c74f13f-fa83-4c48-9b33-68921dd72463'
- *   'The roof, the roof, the roof is on fire'
- *
- * @return {RegExp}
- */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  return /^{\d[a-fA-f\d]{7}-[a-fA-f\d]{4}-[a-fA-f\d]{4}-[a-fA-f\d]{4}-\d[a-fA-f\d]{11}}$/;
 }
 
 
--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -6,30 +6,16 @@
  ************************************************************************************************ */
 
 
-/**
- * Return Promise object that is resolved with string value === 'Hooray!!! She said "Yes"!',
- * if boolean value === true is passed, resolved with string value === 'Oh no, she said "No".',
- * if boolean value === false is passed, and rejected
- * with error message === 'Wrong parameter is passed! Ask her again.',
- * if is not boolean value passed
- *
- * @param {boolean} isPositiveAnswer
- * @return {Promise}
- *
- * @example
- *    const p1 = willYouMarryMe(true);
- *    p1.then(answer => console.log(answer)) // 'Hooray!!! She said "Yes"!'
- *
- *    const p2 = willYouMarryMe(false);
- *    p2.then(answer => console.log(answer)) // 'Oh no, she said "No".';
- *
- *    const p3 = willYouMarryMe();
- *    p3.then(answer => console.log(answer))
- *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
- *                                                    //  Ask her again.';
- */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (isPositiveAnswer === true) {
+      resolve('Hooray!!! She said "Yes"!');
+    } else if (isPositiveAnswer === false) {
+      resolve('Oh no, she said "No".');
+    } else {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+  });
 }
 
 
